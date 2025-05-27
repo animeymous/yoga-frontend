a1bb9407fc7f95492d8870e43e0887b4f2d08036
Rahul.Kumar <rahul.kumar@tible.com>
2025-05-27 17:51:58 +0530

    yoga-1: added contact form left side photo

--- src/components/sections/contact/ContactSection.tsx
+++ src/components/sections/contact/ContactSection.tsx
@@ -11,7 +11,10 @@ export default function ContactSection() {
     return (
       <>
         <section className="breadcrumb-section w-full h-[40vh]">
-          <div className="relative w-full h-full bg-no-repeat bg-cover bg-center" style={{ backgroundImage: "url('/images/contact1.jpg')" }}>
+          <div
+            className="relative w-full h-full bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/contact1.jpg')" }}
+          >
             {/* Overlay div with flexbox to center content */}
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4">
               <h2 className="text-3xl font-bold mb-2">Get in Touch</h2>
@@ -24,7 +27,9 @@ export default function ContactSection() {
           <div className="contact-location-section-parent-div mx-10">
             <div className="section-title-block text-center mb-10">
               <div className="section-subtitle">Our Expertise</div>
-              <h2 className="section-title">A comfortable stage for mindful living</h2>
+              <h2 className="section-title">
+                A comfortable stage for mindful living
+              </h2>
             </div>
             <div className="contact-location-wrap grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
               <Card>
@@ -68,13 +73,17 @@ export default function ContactSection() {
         </section>
         <section className="contact-us-form-section pb-16">
           <div className="contact-us-form-section-parent-div mx-10">
-            <div className="contact-us-form-area">
-              <div className="contact-us-form-wrap">
-                this will have form
-              </div>
+            <div className="contact-us-form-area grid grid-cols-1 lg:grid-cols-2 gap-8">
+              <div className="contact-us-form-wrap">this will have form</div>
               <div className="contact-us-info-wrap">
                 <div className="contact-us-info-area">
-                  this one will have image and some texts in center of it just like we did top of the page 
+                  <div
+                    className="relative w-full max-w-[100vw] sm:max-w-[96vw] md:max-w-[696px] lg:max-w-[456px] xl:max-w-[100vw] h-[70vh] bg-no-repeat bg-cover bg-center mx-auto
+    "
+                    style={{ backgroundImage: "url('/images/contact3.jpg')" }}
+                  >
+                    {/* Overlay content will go here later */}
+                  </div>
                 </div>
               </div>
             </div>
