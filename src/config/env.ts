67ddabddf62b26c34759f73086f57a235e3265f2
Rahul.Kumar <rahul.kumar@tible.com>
2025-05-31 23:16:04 +0530

    yoga-1: added console

--- src/config/env.ts
+++ src/config/env.ts
@@ -23,6 +23,7 @@ const prodConfig: EnvConfig = {
 
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8080';
 const apiVersion = 'v1';
+console.log("process.env.ADMIN_USERNAME", process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD);
 const adminUsername = process.env.ADMIN_USERNAME ?? 'admin';
 const adminPassword = process.env.ADMIN_PASSWORD ?? 'admin123';
 
